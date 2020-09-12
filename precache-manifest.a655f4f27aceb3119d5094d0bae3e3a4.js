5df61fd6535f5964d30af6a954e900f61cf4ff2a
Tharsha Loganathan <tharshzlog@gmail.com>
2020-09-12 19:52:58 +0530

    Updates

---
+++ precache-manifest.a655f4f27aceb3119d5094d0bae3e3a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94866d896a3e43d1710200db78a996d",
+    "url": "/First-React-Front-End/index.html"
+  },
+  {
+    "revision": "6ad55a02caa90d9a7be0",
+    "url": "/First-React-Front-End/static/css/main.c4ebfc00.chunk.css"
+  },
+  {
+    "revision": "8689cb7017e3c9331b87",
+    "url": "/First-React-Front-End/static/js/2.9b2ae0ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-React-Front-End/static/js/2.9b2ae0ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad55a02caa90d9a7be0",
+    "url": "/First-React-Front-End/static/js/main.ad472b34.chunk.js"
+  },
+  {
+    "revision": "fe1a6f034915061b48f5",
+    "url": "/First-React-Front-End/static/js/runtime-main.1d4b5b87.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/First-React-Front-End/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/First-React-Front-End/static/media/map-image.f40ed5b1.png"
+  }
+]);
